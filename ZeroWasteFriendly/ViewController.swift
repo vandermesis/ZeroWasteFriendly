118103e366ec02406bb5702b8a2ef70e960a5fbf
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2020-01-14 22:45:18 +0100

    Initial Commit

---
+++ ZeroWasteFriendly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZeroWasteFriendly
+//
+//  Created by Marek Skrzelowski on 14/01/2020.
+//  Copyright © 2020 vandermesis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
