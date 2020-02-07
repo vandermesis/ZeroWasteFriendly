90c22b419cc0e0209a067f0e8ba0d01d905b589a
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2020-02-07 22:47:08 +0100

    Modify predefined FakeWorker

--- ZeroWasteFriendlyTests/ActivityTiles/FakeActivityTilesWorker.swift
+++ ZeroWasteFriendlyTests/ActivityTiles/FakeActivityTilesWorker.swift
@@ -10,7 +10,7 @@
 
 final class FakeActivityTilesWorker: ActivityTilesWorker {
 
-    func fetchActivityTiles() {
-
+    func fetchActivityTiles(completion: FetchActivityTilesCompletion?) {
+        <#code#>
     }
 }
