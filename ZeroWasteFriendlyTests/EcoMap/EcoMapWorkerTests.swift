91c0e04183b3ed50c81b9bb0cdd2456e2de2f7b3
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2020-02-26 00:50:21 +0100

    Add EcoMapWorker unit tests

--- ZeroWasteFriendlyTests/EcoMap/EcoMapWorkerTests.swift
+++ ZeroWasteFriendlyTests/EcoMap/EcoMapWorkerTests.swift
@@ -12,23 +12,122 @@ import XCTest
 
 final class EcoMapWorkerTests: XCTestCase {
 
+    var repository: FakePlacesRepository!
+    var locationServices: FakeLocationService!
+    var worker: EcoMapWorkerImpl!
+    var receivedLocation: Location?
+    var receivedPlaces: [Place]?
+    var receivedError: Error?
+
     override func setUp() {
         super.setUp()
-
+        repository = FakePlacesRepository()
+        locationServices = FakeLocationService()
+        worker = EcoMapWorkerImpl(repository: repository, locationService: locationServices)
     }
 
     override func tearDown() {
-
+        repository = nil
+        locationServices = nil
+        worker = nil
+        receivedLocation = nil
+        receivedError = nil
         super.tearDown()
     }
 
-    func testExample() {
-
+    func testWorkerIsFetchingUserLocation() {
         // Given
-
+        worker.fetchUserLocation { result in
+            switch result {
+            case .success(let location):
+                self.receivedLocation = location
+            case .failure(let error):
+                self.receivedError = error
+            }
+        }
         // When
-
+        locationServices.fetchCurrentLocationCompletion?(.success(Mock.location))
         // Then
-        // XCTAssertEqual()
+        XCTAssertEqual(locationServices.fetchCurrentLocationCalled, true, "calling location services to find user location is true")
+    }
+
+    func testWorkerIsFetchingUserLocationIsSuccess() {
+        // Given
+        worker.fetchUserLocation { result in
+            switch result {
+            case .success(let location):
+                self.receivedLocation = location
+            case .failure(let error):
+                self.receivedError = error
+            }
+        }
+        // When
+        locationServices.fetchCurrentLocationCompletion?(.success(Mock.location))
+        // Then
+        XCTAssertEqual(receivedLocation?.latitude, 50.257564, "worker should receive valid location")
+    }
+
+    func testWorkerIsFetchingUserLocationIsFailure() {
+        // Given
+        worker.fetchUserLocation { result in
+            switch result {
+            case .success(let location):
+                self.receivedLocation = location
+            case .failure(let error):
+                self.receivedError = error
+            }
+        }
+        // When
+        locationServices.fetchCurrentLocationCompletion?(.failure(UnitTestError()))
+        // Then
+        XCTAssert(receivedError is UnitTestError, "worker should receive Error")
+    }
+
+    func testWorkerIsFetchingPlaces() {
+        // Given
+        worker.fetchPlaces { result in
+            switch result {
+            case .success(let places):
+                self.receivedPlaces = places
+            case .failure(let error):
+                self.receivedError = error
+            }
+        }
+        // When
+        repository.fetchPlacesCompletion?(.success(Mock.places))
+        // Then
+        XCTAssertEqual(repository.fetchPlacesCalled, true, "calling repository to fetch places is true")
+    }
+
+    func testWorkerIsFetchingPlacesIsSuccess() {
+        // Given
+        worker.fetchPlaces { result in
+            switch result {
+            case .success(let places):
+                self.receivedPlaces = places
+            case .failure(let error):
+                self.receivedError = error
+            }
+        }
+        // When
+        repository.fetchPlacesCompletion?(.success(Mock.places))
+        // Then
+        XCTAssertEqual(receivedPlaces?[1].name, "Piekarnia Michalski", "worker should receive valid place name")
+    }
+
+    func testWorkerIsFetchingPlacesIsFailure() {
+        // Given
+        worker.fetchPlaces { result in
+            switch result {
+            case .success(let places):
+                self.receivedPlaces = places
+            case .failure(let error):
+                self.receivedError = error
+            }
+        }
+        // When
+        repository.fetchPlacesCompletion?(.failure(UnitTestError()))
+        // Then
+        XCTAssert(receivedError is UnitTestError, "worker should receive Error")
     }
 }
